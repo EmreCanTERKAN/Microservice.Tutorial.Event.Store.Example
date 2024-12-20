661cb99b8cb8c261494c355ea8c66510c46aa44e
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-20 18:11:15 +0300

    Add project files.

---
+++ Event.Store.Example/Program.cs
@@ -0,0 +1,8 @@
+﻿using EventStore.Client;
+
+string connectionString = "tcp://admin:changeit@localhost:1113";
+
+var settings = EventStoreClientSettings.Create(connectionString);
+
+//bağlantıyı sağlayacak olan clienti oluşturduk.
+var client = new EventStoreClient(settings);
